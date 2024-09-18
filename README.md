073e82e2ba3a7810133fc66598324d30610264db
Jean Silva <jeancsil@gmail.com>
2024-09-19 00:38:24 +0200

    More information and support to other OS (#1)
    README.md file is now better organized and explicative.
    
    The install.sh script is a bit more verbose for user's information, as it is only executed once.
    
    More parameters (-h/--help/-v/--version/--pgn) and ways to import games with lichess cmd:  clipboard + any standard input operation

--- README.md
+++ README.md
@@ -1,15 +1,23 @@
 # Lichess PGN Importer
 
-This is a simple tool to import a PGN file into Lichess. It uses the Lichess API to import the game and then prints the URL of the game.  After that, you can use the `lichess` command to open the game in your browser and request a computer analysis.
+Tool to import a PGN (Portable Game Notation) file into [Lichess.com](https://lichess.org/) from the clipboard.
+The program outputs the URL of the imported game so you can request computer analysis for all your games effortlessly and in a matter of seconds.
+
+## Why Use This Tool?
+
+- Easily import chess games from other platforms to your **Lichess** account, **game analysis**, etc...
+- Streamline your workflow for reviewing and analyzing chess games from different sources.
 
 ## Prerequisites
 
-- [Docker](https://www.docker.com/get-started)
-- [Lichess.org API Token](https://lichess.org/account/oauth/token) | [+info](https://lichess.org/api#section/Authentication)
+- [Docker](https://www.docker.com/get-started) (version 20.10 or later recommended)
+- [Lichess](https://lichess.org) account and a [Personal API access tokens](https://lichess.org/account/oauth/token)
 
 ## Quick Install
 
-Run this command to install the Lichess PGN Importer:
+Don't worry, there is a [quick uninstall](#uninstall) as well 🪦
+
+Run this command to install the Lichess PGN Importer (always check scripts before running it! This one is pretty safe/simple):
 
 ```bash
 curl -sSL https://raw.githubusercontent.com/jeancsil/lichess/main/install.sh | bash
@@ -18,10 +26,10 @@ curl -sSL https://raw.githubusercontent.com/jeancsil/lichess/main/install.sh | b
 This script will:
 
 1. Check if the necessary executables are installed (`Docker`, `pbpaste`)
-2. Pull the necessary Docker image
+2. Pull the Docker image
 3. Set up the `lichess` command for easy execution
 
-After installation, make sure to set your Lichess API token as an environment variable.
+After installation, make sure to set your Lichess API token as an [environment variable](#configuration).
 
 ## Configuration
 
@@ -71,3 +79,4 @@ Alternatively, if you have cloned the repository, you can run:
 ```bash
 make uninstall
 ```
+
--- lichess.sh
+++ lichess.sh
@@ -1,9 +1,78 @@
 #!/usr/bin/env bash 
-
 set -e  # Exit immediately if a command exits with a non-zero status.
 
+# The VERSION will be replaced during the installation or update process
+VERSION="{{VERSION}}"
+
+show_help() {
+    echo "Imports a PGN from clipboard or stdin to Lichess."
+    echo ""
+    echo -e "Usage:"
+    echo -e "\tlichess:\t\t\tTo import a PGN from clipboard"
+    echo -e "\tlichess < /path/to/game.pgn:\tTo import a PGN from a file"
+    echo -e "\tcat /path/to/game.pgn | lichess\tTo import a PGN from a pipe"
+    echo -e "\techo '[PGN data]' | lichess\tTo import a PGN from stdin"
+
+    echo
+    echo "Options:"
+    echo "  -h, --help     Display this help message and exit"
+    echo "  -v, --version  Display version information and exit"
+    echo "  -p, --pgn      Displays an example PGN with valid and accepted headers for reference"
+    echo
+}
+
+show_version() {
+    echo "Lichess PGN Importer v$VERSION"
+}
+
+show_pgn() {
+    echo "[Event \"Opera Game\"]"
+    echo "[Site \"Paris FRA\"]"
+    echo "[Date \"1858.??.??\"]"
+    echo "[Round \"?\"]"
+    echo "[White \"Morphy, Paul\"]"
+    echo "[Black \"Duke of Brunswick & Count Isouard\"]"
+    echo "[Result \"1-0\"]"
+    echo "[ECO \"C41\"]"
+    echo "[PlyCount \"17\"]"
+    echo "[EventDate \"1858.??.??\"]"
+    echo ""
+    echo -n "1. e4 e5 2. Nf3 d6 3. d4 Bg4 4. dxe5 Bxf3 5. Qxf3 dxe5 6. Bc4 Nf6 7. Qb3 Qe7"
+    echo -n "8. Nc3 c6 9. Bg5 b5 10. Nxb5 cxb5 11. Bxb5+ Nbd7 12. O-O-O Rd8"
+    echo  "13. Rxd7 Rxd7 14. Rd1 Qe6 15. Bxd7+ Nxd7 16. Qb8+ Nxb8 17. Rd8# 1-0"
+}
+
+# Check for arguments
+if [ "$#" -gt 0 ]; then
+    case "$1" in
+        -h|--help)
+            show_help
+            exit 0
+            ;;
+        -v|--version)
+            show_version
+            exit 0
+            ;;
+        -p|--pgn)
+            show_pgn
+            exit 0
+            ;;
+        *)
+            echo "Unknown option: $1"
+            echo "Use 'lichess --help' for usage information."
+            exit 1
+            ;;
+    esac
+fi
+
 # Get PGN from clipboard
-PGN=$(pbpaste)
+if [ -t 0 ]; then
+    # Terminal is interactive, use clipboard
+    PGN=$(pbpaste)
+else
+    # Data is being piped in, read from stdin
+    PGN=$(cat)
+fi
 
 # Check if clipboard is empty or PGN doesn't have headers
 if [ -z "$PGN" ] || [[ ! $PGN =~ ^\[Event ]]; then
